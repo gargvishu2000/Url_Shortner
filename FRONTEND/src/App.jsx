85003569ad26cb58cb54cdea902e95bc04fa9b14
gargvishu2000 <45060213+gargvishu2000@users.noreply.github.com>
2025-06-19 14:38:24 +0530

    App.js changed

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -3,7 +3,7 @@ import HomePage from './pages/HomePage';
 import AuthPage from './pages/AuthPage';
 import { Outlet } from '@tanstack/react-router'
 import Navbar from './components/Navbar'
-import LoginForm from './components/loginForm';
+import LoginForm from './components/LoginForm';
 
 
 const App = () => {
