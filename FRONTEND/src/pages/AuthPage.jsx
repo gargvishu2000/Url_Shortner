4e89ba218357d495f7449b2408ec1e8dbba23ed9
gargvishu2000 <45060213+gargvishu2000@users.noreply.github.com>
2025-06-19 14:39:38 +0530

    added

--- FRONTEND/src/pages/AuthPage.jsx
+++ FRONTEND/src/pages/AuthPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import LoginForm from '../components/loginForm'
+import LoginForm from '../components/LoginForm'
 import { useState } from 'react'
-import RegisterForm from '../components/registerForm';
+import RegisterForm from '../components/RegisterForm';
 
 const AuthPage = () => {
 
